de61b20e93b35a77a6044da012d3100f5d4ffde1
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2023-09-18 18:28:05 +0530

    18th September HW

---
+++ C Programming/18th September/swap.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	
+	int a, b;
+	printf("Enter the first integer: ");
+	scanf("%d", &a);
+	printf("Enter the second integer: ");
+	scanf("%d", &b);
+	      
+	printf("Before swap a=%d b=%d",a,b);      
+	a+=b;
+	b=a-b;
+	a-=b;
+	printf("\nAfter swap a=%d b=%d",a,b);    
+	
+//	printf("\nBefore swapping\nthe value of 'a' is %d\nand the value of 'b' is %d", a, b);
+//	int c = b;
+//	b = a;
+//	a = c;
+//	
+//	printf("\nBefore swapping\nthe value of 'a' is %d\nand the value of 'b' is %d", a, b);
+	
+	return 0;
+}
