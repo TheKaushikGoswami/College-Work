5f9019abacdd3ca7c1e02d770f0a7292382f1938
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2023-09-19 12:04:08 +0530

    19th September CW

---
+++ C Programming/19th September/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 0112;
+    printf("%x", a);
+
+    #define pi 3.14; // this method takes 0 bytes of size
+
+    /*
+    %x = hexadecimal
+    %o = octal
+    */
+    
+    return 0;
+}
