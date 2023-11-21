0623473ab98edbb1cd09da1260d7b7ea3ae83da1
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2023-11-21 12:29:45 +0530

    21st November CW

---
+++ Web Technologies/php/bms/includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/login.css">
+    <link href="https://fonts.googleapis.com/css?family=Gabarito" rel="stylesheet">
