91cd4d7fd35ace6c01c9875ff59e4a8c06082c26
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2023-12-18 12:22:15 +0530

    Update 07th.c

--- C Programming/Practicals/07th.c
+++ C Programming/Practicals/07th.c
@@ -6,7 +6,10 @@ int main(){
     int n1, n2, hcf;
     printf("Enter two numbers (separated by space): ");
     scanf("%d %d", &n1, &n2);
-
+    if (n1 == 0 || n2 == 0){
+        printf("%d", n1+n2);
+        return 0;
+    }
     for(int i = 1; i <= n1 && i <= n2; i++){
         if(n1 % i == 0 && n2 % i == 0){
             hcf = i;
